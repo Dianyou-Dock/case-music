474591cc387bf4425950ef11af69bb82196dbaa9
malefooo <784479660@qq.com>
2024-12-04 10:12:20 +0800

    Mapping Rust data structures to TS

---
+++ core/src/types/playlist.ts
@@ -0,0 +1,9 @@
+import {MusicSource} from "@/types/constants.ts";
+
+
+export interface PlaylistData {
+    type: MusicSource
+    content: object
+}
+
+export interface PlaylistInfo extends PlaylistData {}
