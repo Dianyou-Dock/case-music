004e62dabd64d3d0d11e4d0c126d1e5191d47d1a
cloud <31642966+malefooo@users.noreply.github.com>
2024-12-06 17:07:12 +0800

    feat logout (#27)

--- core/src/components/audio-source/audio-source-card.tsx
+++ core/src/components/audio-source/audio-source-card.tsx
@@ -4,21 +4,49 @@ import { Button } from "@/components/ui/button";
 import { useAudioSource } from "@/hooks/use-audio-source";
 import { cn } from "@/lib/utils";
 import { AuthDialog } from "@/components/auth-dialog.tsx";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import { AudioSource } from "@/lib/audio-sources";
+import {invoke} from "@tauri-apps/api/core";
+import {ApplicationResp} from "@/types/application.ts";
 
 export function AudioSourceCard({ source }: { source: AudioSource }) {
   const { audioSource } = useAudioSource();
   const isSelected = audioSource?.find((s) => s.id === source.id)?.connected;
   const [isAuthDialogOpen, setIsAuthDialogOpen] = useState(false);
+  const [reload, setReload] = useState(false);
+
+  async function logout(): Promise<ApplicationResp<any> | undefined> {
+    try {
+      const res = await invoke<ApplicationResp<any>>("logout", {source: source.id});
+      console.log(res);
+      return res
+    } catch (e) {
+      console.error(e);
+      return undefined
+    }
+  }
 
   const handleConnectClick = () => {
+    console.log("isSelected: ",isSelected);
     // 点击按钮时，打开 AuthDialog
     if (isSelected) {
-      return;
+      logout().then((res) => {
+        if (res != undefined && res.code == 0) {
+          const item = audioSource?.find((s) => s.id === source.id);
+          if (item) {
+            item.connected = false;
+            setReload(true);
+          }
+        }
+      });
+    } else {
+      setIsAuthDialogOpen(true);
     }
-    setIsAuthDialogOpen(true);
   };
+
+  useEffect(() => {
+    console.log("Reloading...");
+  }, [reload]);
 
   return (
     <div
