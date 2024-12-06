18ff41ed6f3f02b10ab54644ad7c9918cafc20d5
Baoyuan <baoyuan.top@gmail.com>
2024-12-06 21:31:55 +0800

    fix: logout state change

--- core/src/components/audio-source/audio-source-card.tsx
+++ core/src/components/audio-source/audio-source-card.tsx
@@ -4,49 +4,49 @@ import { Button } from "@/components/ui/button";
 import { useAudioSource } from "@/hooks/use-audio-source";
 import { cn } from "@/lib/utils";
 import { AuthDialog } from "@/components/auth-dialog.tsx";
-import {useEffect, useState} from "react";
+import { useState } from "react";
 import { AudioSource } from "@/lib/audio-sources";
-import {invoke} from "@tauri-apps/api/core";
-import {ApplicationResp} from "@/types/application.ts";
+import { invoke } from "@tauri-apps/api/core";
+import { ApplicationResp } from "@/types/application.ts";
 
 export function AudioSourceCard({ source }: { source: AudioSource }) {
-  const { audioSource } = useAudioSource();
+  const { audioSource, configureSource } = useAudioSource();
   const isSelected = audioSource?.find((s) => s.id === source.id)?.connected;
   const [isAuthDialogOpen, setIsAuthDialogOpen] = useState(false);
-  const [reload, setReload] = useState(false);
 
   async function logout(): Promise<ApplicationResp<any> | undefined> {
     try {
-      const res = await invoke<ApplicationResp<any>>("logout", {source: source.id});
+      const res = await invoke<ApplicationResp<any>>("logout", {
+        source: source.id,
+      });
       console.log(res);
-      return res
+      return res;
     } catch (e) {
       console.error(e);
-      return undefined
+      return undefined;
     }
   }
 
   const handleConnectClick = () => {
-    console.log("isSelected: ",isSelected);
+    console.log("isSelected: ", isSelected);
     // 点击按钮时，打开 AuthDialog
     if (isSelected) {
       logout().then((res) => {
         if (res != undefined && res.code == 0) {
-          const item = audioSource?.find((s) => s.id === source.id);
-          if (item) {
-            item.connected = false;
-            setReload(true);
-          }
+          // update current audio source connect status
+          const newSource = audioSource?.map((s) => {
+            if (s.id === source.id) {
+              return { ...s, connected: false };
+            }
+            return s;
+          });
+          configureSource(newSource);
         }
       });
     } else {
       setIsAuthDialogOpen(true);
     }
   };
-
-  useEffect(() => {
-    console.log("Reloading...");
-  }, [reload]);
 
   return (
     <div
@@ -61,12 +61,14 @@ export function AudioSourceCard({ source }: { source: AudioSource }) {
           <p className="text-sm text-muted-foreground">{source.desc}</p>
         </div>
       </div>
+
       <Button
-        variant={isSelected ? "default" : "outline"}
+        variant={isSelected ? "outline" : "default"}
         onClick={handleConnectClick}
       >
-        {isSelected ? "Connected" : "Connect"}
+        {isSelected ? "Disconnect" : "Connect"}
       </Button>
+
       <AuthDialog
         isOpen={isAuthDialogOpen}
         setIsOpen={setIsAuthDialogOpen}
